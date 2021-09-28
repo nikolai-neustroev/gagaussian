f21b56790514999a38ec7ba68bef3eb774fc876e
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2021-09-28 15:52:34 +0900

    General distribution parent class

--- gagaussian/general.py
+++ gagaussian/general.py
@@ -1,9 +1,10 @@
 from abc import ABC, abstractmethod
+from typing import NoReturn
 
 
 class DistributionInterface(ABC):
     @abstractmethod
-    def __init__(self, mean, stdev):
+    def __init__(self, mean: float, stdev: float):
         raise NotImplementedError
 
     @abstractmethod
@@ -15,7 +16,7 @@ class DistributionInterface(ABC):
         raise NotImplementedError
 
     @abstractmethod
-    def read(self, file):
+    def read(self, file: str) -> NoReturn:
         raise NotImplementedError
 
     @abstractmethod
@@ -37,3 +38,35 @@ class DistributionInterface(ABC):
     @abstractmethod
     def plot_pdf(self):
         raise NotImplementedError
+
+
+class Distribution(DistributionInterface, ABC):
+    def __init__(self, mean=0., stdev=1.):
+        """General distribution parent class.
+
+        Args:
+            mean (float): The mean value of the distribution.
+            stdev (float): The standard deviation of the distribution.
+        """
+        self.mean = mean
+        self.stdev = stdev
+        self.data = []
+
+    def read(self, file: str) -> NoReturn:
+        """Method to read data from a text file.
+
+        Args:
+            file (str): Name of a file to read.
+
+        Returns:
+            NoReturn
+        """
+        with open(file) as file:
+            data = []
+            line = file.readline()
+            with line:
+                data.append(int(line))
+                line = file.readline()
+        file.close()
+
+        self.data = data
