4c2b987fe509bef5f455a876a65d1f57db6d83b9
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2021-10-06 23:07:45 +0900

    Distribution unit tests init commit

---
+++ tests/test_general.py
@@ -0,0 +1,21 @@
+from unittest import TestCase
+
+
+class TestDistribution(TestCase):
+    def test_set_mean(self):
+        self.fail()
+
+    def test_get_mean(self):
+        self.fail()
+
+    def test_set_stdev(self):
+        self.fail()
+
+    def test_get_stdev(self):
+        self.fail()
+
+    def test_get_data(self):
+        self.fail()
+
+    def test_read(self):
+        self.fail()
